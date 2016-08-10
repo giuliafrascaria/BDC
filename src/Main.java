68c852b4ea988f141bca123b75b0f9ca58ee08fd
giogge <giogge@localhost.localdomain>
2016-08-10 16:55:37 +0200

    commit di giulia

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import boundary.LoginGUI;
 
 public class Main {
 
+	if (args[1].equals("")) {
+ 			System.out.println("ciao sono giulia");
+ 		}
+
 	public static void main(String[] args) {
 		new LoginGUI();
 	}
